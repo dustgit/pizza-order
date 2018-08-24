36e68b55649699e3915b20a00655913099a35fcc
Dustin Starke <dustin.starke@gmail.com>
2018-08-24 13:50:12 -0700

    complete a mvp

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,40 @@
 // business logic
-function Pizza(name, size, toppings) {
+function Pizza(name, size, toppings, basePrice) {
   this.name = name;
   this.size = size;
   this.toppings = toppings;
+  this.basePrice = 10;
 }
 
-function Toppings(fourCheese, onionBrie, crimini, spinach, olives, carnage) {
-  this.fourCheese = fourCheese;
-  this.onionBrie = onionBrie;
-  this.crimini = crimini;
-  this.spinach = spinach;
-  this.olives = olives;
-  this.carnage = carnage;
+Pizza.prototype.pricing = function() {
+
+  if (this.size === "fifteenInch") {
+    this.basePrice += 5;
+  } else if (this.size === "twentyInch") {
+    this.basePrice += 10;
+  }
+  this.baseprice += this.toppings.length;
+  return this.baseprice;
 }
-function Size(twelve, fifteen, twenty) {
-  this.twelve = 12;
-  this.fifteen = 15;
-  this.twenty = 20;
-}
-
-Pizza.prototype.pizzaCost = function(toppingsNumber) {
-
-  var total = (toppingsNumber + 3)
-  return total;
-}
-
-
-
-
-
-
-
 
 
 // user interface logic
 $(document).ready(function() {
   $("form#pizzaForm").submit(function(event) {
     event.preventDefault();
-    var userName = $("input#userName").val();
-    var pizzaSize = $("input.pizzaSize").val();
-    var pizzaToppings = $(".toppings:checkbox:checked").val();
-    console.log(userName + " " + pizzaSize + " " + pizzaToppings);
+    $("#orderReceipt").show();
+
+    var inputName = $("input#userName").val();
+    var inputSize = $("#sizeSelected").val();
+    var inputToppings = [];
+     $("input:checkbox:checked").each() {inputToppings.push($(this).val());}
+    console.log(inputName + " " + inputSize + " " + inputToppings);
+
+    var onePizza = new Pizza(inputSize, inputToppings);
+    var total = newPizza.pricing();
+    $(".pizzaSize").text(newPizza.pizzaSize);
+    $("#displaytotal").text(newPizza.basePrice);
+    $("#orderReceipt").show();
+
   });
+});
