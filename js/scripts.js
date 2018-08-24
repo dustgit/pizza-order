39e015caba9c3e5e7d8f49716f278a5d7018d4a8
Dustin Starke <dustin.starke@gmail.com>
2018-08-24 10:03:26 -0700

    create beginning html layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+// business logic
+function Pizza(name, size, toppings) {
+  this.name = name;
+  this.pizzaSize = size;
+  this.toppings = toppings;
+}
+Pizza.prototype.pricing = function() {
+  if (this.when == "oldMovie") {
+    return "4"
+  } else if (this.age == "kidPrice") {
+    return "5"
+  } else if (this.age == "seniorPrice") {
+    return "7"
+  } else if (this.time == "beforeFive") {
+    return "8"
+  } else {
+    return(10) }
+  }
+
+
 $(document).ready(function() {
-  $("form#pizzaType").submit(function(event) {
+  $("form#pizza-order").submit(function(event) {
     event.preventDefault();
+    var userName = $("input#name").val();
+    var pizzaSize = $("input#size").val();
+    var pizzaToppings = $()
