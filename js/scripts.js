1dc0a1fc1ebfaf04de93a0a465ae80f0aa0663b0
Dustin Starke <dustin.starke@gmail.com>
2018-08-24 10:34:39 -0700

    add functionality to toppings and create price formula

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
 // business logic
 function Pizza(name, size, toppings) {
   this.name = name;
-  this.pizzaSize = size;
+  this.size = size;
   this.toppings = toppings;
 }
-Pizza.prototype.pricing = function() {
-  if (this.when == "oldMovie") {
-    return "4"
-  } else if (this.age == "kidPrice") {
-    return "5"
-  } else if (this.age == "seniorPrice") {
-    return "7"
-  } else if (this.time == "beforeFive") {
-    return "8"
-  } else {
-    return(10) }
-  }
+
+function Toppings(fourCheese, onionBrie, crimini, spinach, olives, carnage) {
+  this.fourCheese = fourCheese;
+  this.onionBrie = onionBrie;
+  this.crimini = crimini;
+  this.spinach = spinach;
+  this.olives = olives;
+  this.carnage = carnage;
+}
+function Size(twelve, fifteen, twenty) {
+  this.twelve = 12;
+  this.fifteen = 15;
+  this.twenty = 20;
+}
+
+Pizza.prototype.pizzaCost = function(toppingsNumber) {
+
+  var total = (toppingsNumber + 3)
+  return total;
+}
 
 
+
+
+
+
+
+
+
+// user interface logic
 $(document).ready(function() {
-  $("form#pizza-order").submit(function(event) {
+  $("form#pizzaForm").submit(function(event) {
     event.preventDefault();
     var userName = $("input#name").val();
     var pizzaSize = $("input#size").val();
